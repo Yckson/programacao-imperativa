df5921b30bf4e605b47568158bdb185c3f75b086
William Gabriel Braga <70717232+Yckson@users.noreply.github.com>
2024-01-20 15:16:13 -0300

    Alterações na questão 4

--- Will/pratica03/q4.c
+++ Will/pratica03/q4.c
@@ -2,27 +2,38 @@
 
 int obterInverso(int num){
     int unidade; int dezena; int centena; int copiaNum;
+    if (num > 999 || num < 100){
+        return 0;
+        /*O "zero" serve como uma alternativa para o "false", para que quando o valor informado não as métricas exigidas, a função que verifica o tamanho entenda que isso é um erro e retorne o valor 'E'.*/
+    }
+    else{
+        centena = num / 100;
+        copiaNum = num - centena * 100;
 
-    centena = num / 100;
-    copiaNum = num - centena * 100;
+        dezena = copiaNum / 10;
+        copiaNum = copiaNum - dezena * 10;
 
-    dezena = copiaNum / 10;
-    copiaNum = copiaNum - dezena * 10;
+        unidade = copiaNum;
 
-    unidade = copiaNum;
-
-    return unidade * 100 + dezena * 10 + centena;
+        return unidade * 100 + dezena * 10 + centena;
+    }
 }
 
 char verificarTamanho(int num1, int num2){
-    if (num1 < num2){
+    /*Como, se tudo estiver inserido corretamente, o valor invertido será sempre maior que 0, a função será capaz de identificar o erro caso o valor do parâmetro do segundo numero seja igual à 0*/
+    if (num2){
+        if (num1 < num2){
         return 'm';
-    }
-    else if (num1 > num2){
-        return 'M';
+        }
+        else if (num1 > num2){
+            return 'M';
+        }
+        else{
+            return 'I';
+        }
     }
     else{
-        return 'I';
+        return 'E';
     }
 
 }
@@ -48,7 +59,7 @@ int main (){
             printf("\nNumero gerado igual ao numero lido (%d = %d).", numero, numeroInvertido);
             break;
         default:
-            printf("Erro ao computar os numeros! Verifique o numero fornecido e tente novamente");
+            printf("Erro ao computar os numeros! Verifique o numero fornecido e tente novamente.");
     }
     
 
