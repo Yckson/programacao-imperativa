8aa93eaf16c3f16d1d3f9e2408f127287bb0e5a9
William Gabriel Braga <70717232+Yckson@users.noreply.github.com>
2024-01-29 08:19:48 -0300

    Questão 3 prática 4

--- Will/pratica04/q3.c
+++ Will/pratica04/q3.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
+#include <ctype.h>
 
 
 int main (){
+    int media = 7;
+    char continuar;
+    float nota1;
 
+    do{
+        printf("\nDigite a nota do aluno: ");
+        scanf("%f", &nota1);
+
+        if (nota1 >= media){
+            printf("\nAluno aprovado!");
+        }
+        else{
+            if (nota1 > 3 && media != 5){
+                media = 5;
+                printf("\nAluno em recuperacao!");
+                continue;
+            }
+            else{
+                printf("\nAluno reprovado!");
+            }
+        }
+        media = 7;
+        printf("\nDeseja continuar (S/N): ");
+        scanf(" %c", &continuar);
+        if(toupper(continuar) != 'S'){
+            break;
+        }
+
+
+
+    }
+    while (1);
 
 
 
